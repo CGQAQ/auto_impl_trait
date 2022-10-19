ffc7dd8458ee5b2d7f0dd4d5800e3c323143bf52
CGQAQ <m.jason.liu@outlook.com>
2022-10-19 08:32:50 +0800

    chores: 0.3.1 add doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,50 @@ impl syn::parse::Parse for Trait {
     }
 }
 
+
+///
+/// This crate is for internal use only.
+///
+/// ```ignore
+/// #[auto_impl_trait("./src/rect_trait.rs", Rect)]
+/// #[doc = "Test this will keep after expand"]
+/// #[derive(Debug)]
+/// struct Square {
+///     side: i32,
+/// }
+/// ```
+///
+/// will expand to
+///
+/// ```ignore
+/// mod item;
+/// mod area;
+/// mod perimeter;
+/// mod scale;
+/// mod ____CGQAQ__SUPER_TRAIT____ {
+///     use std::ops::{Add, Sub, Mul, Div};
+///
+///     pub trait Rect {
+///         type Item: Add + Sub + Mul + Div;
+///         fn area(&self) -> Self::Item;
+///         fn perimeter(&self) -> Self::Item;
+///         fn scale(&mut self, scale: Self::Item);
+///     }
+/// }
+/// use ____CGQAQ__SUPER_TRAIT____::Rect;
+/// #[doc = "Test this will keep after expand"]
+/// #[derive(Debug)]
+/// struct Square {
+///     side: i32,
+/// }
+/// impl Rect for Square {
+///     type Item = crate::item::Item;
+///     fn area(&self) -> Self::Item { <dyn crate::area::Area>::area(self) }
+///     fn perimeter(&self) -> Self::Item { <dyn crate::perimeter::Perimeter>::perimeter(self) }
+///     fn scale(&mut self, scale: Self::Item) { <dyn crate::scale::Scale>::scale(self, scale) }
+/// }
+/// ```
+///
 #[proc_macro_attribute]
 pub fn auto_impl_trait(attr: TokenStream, item: TokenStream) -> TokenStream {
     // Construct a string representation of the type definition
